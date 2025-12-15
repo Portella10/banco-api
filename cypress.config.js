e037fd3b64eebc5be095907f8170b339548fd435
João Portella <{jvportella.m@gmail.com}>
2025-12-15 18:26:48 -0300

    colocando baseUrl no cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
     reporter: "cypress-mochawesome-reporter",
     e2e: {
+        baseUrl: Cypress.env("baseUrl"),
         setupNodeEvents(on, config) {
             require("cypress-mochawesome-reporter/plugin")(on);
         },
